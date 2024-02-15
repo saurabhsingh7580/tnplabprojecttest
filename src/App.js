681f1173e0dbbf32a0309c1aca0566528af9116f
saurabh singh <singhsaurabh758082@gmail.com>
2024-02-16 00:15:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Login from './auth/Login/Login'
+import Signup from './auth/Signup/Signup'
+import Dashboard from './Component/Dashboard'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/home' element={<Dashboard/>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Component/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import SidebarMenu from './SidebarMenu'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Navbar/>
+        <SidebarMenu/>
+    </div>
+  )
+}
+
+export default Dashboard
