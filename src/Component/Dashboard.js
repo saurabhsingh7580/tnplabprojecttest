b762acc4c421e96d0ecd9df201f03759d21f8d93
saurabh singh <singhsaurabh758082@gmail.com>
2024-02-20 23:57:21 +0530

    some changes

--- src/Component/Dashboard.js
+++ src/Component/Dashboard.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import SidebarMenu from './SidebarMenu'
+import { Button } from 'react-bootstrap'
 
 const Dashboard = () => {
+
+
+
+const [data,setData] = useState("tnplab")
+ useEffect(()=>{
+  setData("hiiiiii")
+ },[])
+
   return (
     <div>
         <Navbar/>
