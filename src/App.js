86672fa2120ffe3c2385c46b1bc12590bfe4ed1a
Shivam8319423 <shivamelectronics1992@gmail.com>
2024-02-20 23:57:31 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import Login from './auth/Login/Login'
+import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Login from './auth/Login/Login';
 import Signup from './auth/Signup/Signup'
 import Dashboard from './Component/Dashboard'
 
@@ -8,8 +8,8 @@ const App = () => {
   return (
     <div>
      <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<Login/>}/>
+     <Routes> 
+      <Route  path='/' element={<Login/>}/>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/home' element={<Dashboard/>}/>
      </Routes>
