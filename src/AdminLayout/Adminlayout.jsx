7a41c86be18d207175b379da0deff2bb965c6d9d
Sachin Thakur <65484874+Sachinthakur147@users.noreply.github.com>
2024-02-21 00:38:16 +0530

    second commit

---
+++ src/AdminLayout/Adminlayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dashboard from '../dashboard/Dashboardlist'
+
+const Adminlayout = () => {
+  return (
+    <div>
+      <Dashboard/>
+    </div>
+  )
+}
+
+export default Adminlayout
--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Dashboard from './dashboard/Dashboardlist';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Adminlayout from './AdminLayout/Adminlayout';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <Adminlayout/>
     </div>
   );
 }
